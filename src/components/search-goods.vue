4e1b7b9a8dd4a4fa5db582c283fe0eae1218970a
dkimd5 <dkimd5@mail.ru>
2021-04-15 11:16:42 +0300

    changing search

--- src/components/search-goods.vue
+++ src/components/search-goods.vue
@@ -1,20 +1,20 @@
 <template>
-  <div class="search-wrap">
-    <input type="text" v-model="sLine" class="goods-search" />
-    <button
-      class="search-button"
-      type="button"
-      v-on:click="filterGoods(searchLine)"
-    >
-      Искать
-    </button>
+  <div>
+    <form @filterGoods="filterGoods" class="search-wrap">
+      <input type="text" v-model="sLine" class="goods-search" />
+      <button class="search-button" type="submit">Искать</button>
+    </form>
+    <form>
+      <input type="text" v-model="sLine2" />
+      <button type="button" v-on:click="filterGoods2">Search</button>
+    </form>
   </div>
 </template>
 
 <script>
 export default {
   props: {
-    searchLine: String,
+    // searchLine: String,
     filteredGoods: Array,
     goods: Array,
     good: {
@@ -25,17 +25,24 @@ export default {
 
   data() {
     return {
-      fGoods: this.filteredGoods,
-      sLine: this.searchLine,
+      // fGoods: this.filteredGoods,
+      sLine: "",
+      sLine2: "",
     };
   },
 
   methods: {
-    filterGoods(good_name) {
-      console.log("starting");
-      const regexp = new RegExp(good_name, "i");
-      this.fGoods = this.goods.filter((good) => regexp.test(good.product_name));
+    filterGoods() {
+      console.log(this.sLine);
     },
+    filterGoods2() {
+      console.log(this.sLine2);
+    },
+    // filterGoods(good_name) {
+    //   console.log("starting");
+    //   const regexp = new RegExp(good_name, "i");
+    //   this.fGoods = this.goods.filter((good) => regexp.test(good.product_name));
+    // },
   },
 };
 </script>
